95dff74dd29d5c8377a6ce1b34557e407338540c
Konstantinos Maninakis <maninak@protonmail.com>
2020-04-20 11:50:00 +0200

    refactor: extract logic from main App cmpt

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   IonPage,
   IonContent,
   IonHeader,
   IonTitle,
   IonToolbar,
-  IonItem,
-  IonInput,
 } from '@ionic/react';
-
-import './App.css';
-
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -28,56 +23,11 @@ import '@ionic/react/css/core.css';
 // import '@ionic/react/css/flex-utils.css';
 // import '@ionic/react/css/display.css';
 
+import './App.css';
+import { Repos } from './components/Repos';
 
-import { useHttp } from './hooks/http'
 
 function App() {
-  const [searchKey, setSearchKey] = useState('react')
-  const { fetchedData } = useHttp(`https://api.github.com/search/repositories?q=${searchKey}`, [setSearchKey])
-
-  const repositories = fetchedData
-    ? fetchedData.items
-    : []
-
-  function renderSearchInput() {
-    return (
-      <IonItem>
-        <IonInput
-          type="search"
-          inputmode="search"
-          placeholder="Search repositories..."
-          onIonChange={e => setSearchKey(e.detail.value.trim())}
-          debounce={300}
-          autofocus
-          clearInput
-        />
-      </IonItem>
-    )
-  }
-
-  function renderSearchResults() {
-    return (
-      <>
-        <IonTitle>Search results</IonTitle>
-        <table>
-          <thead>
-            <tr>
-              <th align='left'>Name</th>
-              <th align='left'>Stars</th>
-            </tr>
-          </thead>
-          <tbody>
-          {repositories.map(repo => (
-            <tr key={repo.id}>
-              <td>{repo.full_name}</td>
-              <td>{repo.stargazers_count}</td>
-            </tr>
-          ))}
-          </tbody>
-        </table>
-      </>
-    )
-  }
 
   return (
     <div className="App">
@@ -88,8 +38,7 @@ function App() {
           </IonToolbar>
         </IonHeader>
         <IonContent>
-          {renderSearchInput()}
-          {renderSearchResults()}
+          <Repos />
         </IonContent>
       </IonPage>
     </div>
---
+++ src/components/Repos.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import {
+  IonTitle,
+  IonItem,
+  IonInput,
+} from '@ionic/react';
+
+import { useHttp } from '../hooks/http'
+
+export function Repos() {
+  const [searchKey, setSearchKey] = useState('react')
+  const { fetchedData } = useHttp(`https://api.github.com/search/repositories?q=${searchKey}`, [setSearchKey])
+
+  const repositories = fetchedData
+    ? fetchedData.items
+    : []
+
+  function renderSearchInput() {
+    return (
+      <IonItem>
+        <IonInput
+          type="search"
+          inputmode="search"
+          placeholder="Search repositories..."
+          onIonChange={e => setSearchKey(e.detail.value.trim())}
+          debounce={300}
+          autofocus
+          clearInput
+        />
+      </IonItem>
+    )
+  }
+
+  function renderSearchResults() {
+    return (
+      <>
+        <IonTitle>Search results</IonTitle>
+        <table>
+          <thead>
+            <tr>
+              <th align='left'>Name</th>
+              <th align='left'>Stars</th>
+            </tr>
+          </thead>
+          <tbody>
+          {repositories.map(repo => (
+            <tr key={repo.id}>
+              <td>{repo.full_name}</td>
+              <td>{repo.stargazers_count}</td>
+            </tr>
+          ))}
+          </tbody>
+        </table>
+      </>
+    )
+  }
+  return (
+    <>
+      {renderSearchInput()}
+      {renderSearchResults()}
+    </>
+  )
+}
