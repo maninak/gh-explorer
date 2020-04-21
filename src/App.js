5d69ebe48734709073b136402a1cf51705c03599
Konstantinos Maninakis <maninak@protonmail.com>
2020-04-21 13:31:28 +0200

    fix(App): support deep links in deployed app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
+import { IonReactHashRouter } from '@ionic/react-router';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -31,13 +31,13 @@ export function App() {
 
   return (
     <IonApp className={`${colorScheme === 'dark' ? 'dark-theme' : ''}`}>
-      <IonReactRouter>
+      <IonReactHashRouter>
         <IonRouterOutlet id="main">
           <Route path="/repos" component={Repos} exact={true} />
           <Route path="/repos/:repoId" render={Repo} />
-          <Redirect to="/repos" />
+          <Redirect to="repos" />
         </IonRouterOutlet>
-      </IonReactRouter>
+      </IonReactHashRouter>
     </IonApp>
   );
 }
